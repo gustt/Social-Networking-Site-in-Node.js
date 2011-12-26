1b6097d5fceef1c274ce4f642b56432973aefeaa
darhamid <info@pragtech.co.in>
2011-12-26 19:43:42 +0530

    added the code to make the application work on mobile device and provide the mobile view of the application

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.dynamicHelpers(require('./helpers.js').dynamicHelpers);
 
 
 app.configure('development', function() {
-  app.set('db-uri', 'mongodb://localhost/nodejs_social_network_new');
+  app.set('db-uri', 'mongodb://localhost/nodejs_social_network_new-23');
   app.use(express.errorHandler({ dumpExceptions: true }));
 });
 
@@ -512,6 +512,68 @@ app.get('/userinfo',loadUser, function(req, res){
   })
 });
 
+//Edit User
+
+// update blog post
+app.put('/user/edit/:id', loadUser, function(req, res, next) {
+  User.findById(req.params.id, function(err, user) {
+     function userUpdateFailed() {
+        req.flash('error', 'Failed To Update The User!.. Please Try Again');
+        res.render('users/edit', {
+          locals: {
+            user: user,currentUser:req.currentUser
+          }
+        });
+      }
+    console.log('user id======================'+user.id);
+    console.log('current user id======================'+req.currentUser.id);
+    console.log('request======================'+req.body.user.first_name);
+    if (!user)
+      return next(new NotFound('User Not Found'));
+    else {
+           user.first_name = req.body.user.first_name 
+           user.last_name =  req.body.user.last_name
+           user.age =  req.body.user.age
+           user.sex =  req.body.user.sex
+           user.location =  req.body.user.location
+           var sensor = false;
+           var address = req.body.user.location;
+           geo.geocoder(geo.google, address, sensor,function(formattedAddress, latitude, longitude) {
+    	     console.log("Formatted Address: " + formattedAddress);
+    	     console.log("Latitude: " + latitude);
+    	     console.log("Longitude: " + longitude);
+    	     user.latitude = latitude;
+    	     user.longitude = longitude;
+             console.log('user sex======================'+user.sex);
+             user.save(function(err) {
+               if(err)
+                 return userUpdateFailed();
+               req.flash('info', 'Succesfully Upadated Changes');
+               res.redirect('/userinfo');
+             });
+          });
+       }
+  });
+});
+
+
+app.get('/user/edit/:id', loadUser, function(req, res, next) {
+  User.findById(req.params.id, function(err, user) {
+    if (!user)
+      return next(new NotFound('User could not be found'));
+    else {
+      res.render('users/edit', {
+        locals: {
+          user: user,currentUser: req.currentUser
+
+        }
+      });
+    }
+  });
+});
+
+
+
 //Create new user
 app.get('/users/new', function(req, res) {
   res.render('users/new.jade', {
@@ -526,6 +588,10 @@ app.post('/users.:format?', function(req, res) {
   
   var sensor = false;
   var address = req.body.user.location;
+  var email = req.body.email;
+  var first_name = req.body.first_name;
+  user.email = email;
+  console.log('email id ============================================='+user.email);
   geo.geocoder(geo.google, address, sensor,function(formattedAddress, latitude, longitude) {
     console.log("Formatted Address: " + formattedAddress);
     console.log("Latitude: " + latitude);
@@ -542,7 +608,6 @@ app.post('/users.:format?', function(req, res) {
   }
   user.save(function(err) {
     if (err) return userSaveFailed();
-
     req.flash('info', 'Your account has been created');
     emails.sendWelcome(user);
 
@@ -561,18 +626,29 @@ app.post('/users.:format?', function(req, res) {
 
 // Sessions
 app.get('/sessions/new', function(req, res) {
-  res.render('sessions/new.jade', {
-    locals: { user: new User() }
-  });
+   
+  var ua = req.headers['user-agent'].toLowerCase();
+	if(/android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4))) {
+		res.render('Social/home', {
+                locals: { user: new User() }
+              });
+	}
+        else {
+              res.render('sessions/new.jade', {
+                locals: { user: new User() }
+              });
+             }
+
 });
 
 app.post('/sessions', function(req, res) {
+  console.log('========================================================================inside sessions');
   User.findOne({ email: req.body.user.email }, function(err, user) {
+    console.log('====================users ==================='+user);
     if (user && user.authenticate(req.body.user.password)) {
+       console.log('====================users ==================='+user);
+       console.log('====================users ==================='+user.username);
        req.session.user_id = user.id;
-       var ipAddress = req.connection.remoteAddress;
-       sys.puts('===========================IP Address: ' + ipAddress);
-        
        
       // Remember me
       if (req.body.remember_me) {
@@ -582,11 +658,19 @@ app.post('/sessions', function(req, res) {
           res.redirect('/userinfo');
         });
       } else {
-        res.redirect('/userinfo');
+              
+              var ua = req.headers['user-agent'].toLowerCase();
+	if(/android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4))) {
+              res.render('Social/index');
+	}
+        else {
+              res.redirect('/userinfo');
+             }
       }
     } else {
-      req.flash('error', 'Incorrect credentials');
-      res.redirect('/sessions/new');
+      console.log('invalid username password');
+      //req.flash('error', 'Incorrect credentials');
+      res.redirect('/Social/index');
     }
   }); 
 });
